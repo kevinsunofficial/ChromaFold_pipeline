a2387d8ef3595cc4164e0cd06a328680c43c4625
Yuchen Sun <suny4@lilac-ln02.hpc.private>
2023-09-21 17:51:47 -0400

    init dataloader and metrics definition

---
+++ evaluation/dataloader.py
@@ -0,0 +1,49 @@
+import os
+import os.path as osp
+import numpy as np
+import pandas as pd
+import argparse
+import warnings
+
+warnings.filterwarnings('ignore')
+
+
+def load_pred(pred_dir, ct, chrom, pred_len=200, avg_stripe=False):
+    file = osp.join(pred_dir, ct, 'prediction_{}_chr{}.npz'.format(ct, chrom))
+    temp = np.load(file)['arr_0']
+    chrom_len = temp.shape[0]
+    prep = np.insert(temp, pred_len, 0, axis=1)
+    mat = np.array([
+        np.insert(np.zeros(chrom_len+pred_len+1), i, prep[i]) for i in range(chrom_len)
+    ])
+    summed = np.vstack((
+        np.zeros((pred_len, mat.shape[1])), mat
+    )).T[:chrom_len+pred_len, :chrom_len+pred_len]
+    if avg_stripe:
+        summed = (summed + np.vstack((
+            np.zeros((pred_len, mat.shape[1])), mat
+        ))[:chrom_len+pred_len, :chrom_len+pred_len])/2
+    
+    return summed[pred_len:-pred_len, pred_len:-pred_len]
+
+
+def load_database(gtf_file):
+    pass
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pred_dir', required=True, type=str, help='ChromaFold prediction result directory')
+    parser.add_argument('--ct', required=True, type=str, help='Full cell type name')
+    parser.add_argument('--chrom', required=True, type=int, help='Chromosome number')
+    parser.add_argument('--pred_len', required=False, type=int, default=200, help='Prediction length, default=200')
+    parser.add_argument('--avg_stripe', required=False, action='store_true', help='Average V-stripe, default=False')
+    args = parser.parse_args()
+
+    pred_dir = args.pred_dir
+    ct = args.ct
+    chrom = args.chrom
+    pred_len = args.pred_len
+    avg_stripe = args.avg_stripe
+
+    pred_mat = load_pred(pred_dir, ct, chrom, pred_len=pred_len, avg_stripe=avg_stripe)
---
+++ evaluation/metrics.py
@@ -0,0 +1,42 @@
+import os
+import os.path as osp
+import numpy as np
+import pandas as pd
+import scipy
+import scipy.stats
+import warnings
+
+warnings.filterwarnings('ignore')
+
+
+def topdom(pred_mat, window_size=10):
+    if pred_mat.shape[0]-pred_mat.shape[1]:
+        raise ValueError(
+            'Dimension mismatch ({}, {})'.format(pred_mat.shape[0], pred_mat.shape[1])
+        )
+    pad_mat = np.pad(pred_mat, window_size, mode='constant', constant_values=np.nan)
+    dim = pad_mat.shape[0]
+    signal = np.array([
+        np.nanmean(pad_mat[i-window_size:i, i:i+window_size]) for i in range(dim)
+    ][window_size+1: -window_size])
+
+    return signal
+
+
+def similarity(signal1, signal2, window_size=100):
+    if len(signal1)-len(signal2):
+        raise ValueError(
+            'Different signal1.length and signal2.length'
+        )
+    l = len(signal1)
+    score = np.array([
+        scipy.stats.pearsonr(
+            signal1[i:i+window_size], signal2[i:i+window_size]
+        )[0] for i in range(l-window_size)
+    ])
+
+    return score
+
+
+if __name__=='__main__':
+    pass
