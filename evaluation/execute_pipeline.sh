4b349e6295fbdf539e4391602226ca41d9555147
Yuchen Sun <suny4@lilac-ln02.hpc.private>
2023-10-18 13:36:58 -0400

    modify test cases

--- evaluation/execute_pipeline.sh
+++ evaluation/execute_pipeline.sh
@@ -4,11 +4,11 @@ for ((i=1; i<=19; i++))
 do
     time python pipeline.py --input_dir /data/leslie/suny4/processed_input/ \
                        --pred_dir /data/leslie/suny4/predictions/chromafold/ \
-                       --paired --ct mycGCB_am_gfp_myc_gcb_thelp_sample mycGCB_am_gfp_myc_gcb_nothelp_sample \
+                       --paired --ct darko_wt_cb darko_ctcfhet_cb \
                        --chrom $i --avg_stripe --topdom_window 50 --topdom_cutoff 0 \
                        --kernel diff --pattern smooth --thresh_cutoff 0.1 \
                        --db_file /data/leslie/suny4/data/chrom_size/gencode.vM10.basic.annotation.db \
                        --filters gene_type=protein_coding \
-                       --num_plot 10 --out_dir ../testfile/ \
-                       --fig_dir /data/leslie/suny4/figures/pipeline_test/
+                       --num_plot 10 --out_dir /data/leslie/suny4/figures/darko_wt_ctcfhet/query/ \
+                       --fig_dir /data/leslie/suny4/figures/darko_wt_ctcfhet/
 done
