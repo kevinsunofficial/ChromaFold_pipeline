c9d8e306d00ca49bbaea999553d348003379c0e0
Yuchen Sun <kevin.sun0612@hotmail.com>
2023-10-27 14:28:40 -0400

    add new executions

--- pipeline/execute_pipeline.sh
+++ pipeline/execute_pipeline.sh
@@ -14,8 +14,15 @@ max=19
 
 if [ "$start" -lt "$min" ] || [ "$end" -gt "$max" ]
 then
-    echo "Invalid chromosome range, please try again"
-    exit 1
+    echo "Invalid chromosome range, forcing ranges with min / max"
+    if [ "$start" -lt "$min" ]
+    then
+        start=$min
+    fi
+    if [ "$end" -gt "$max" ]
+    then
+        end=$max
+    fi
 fi
 
 echo "Current sample: $sample"
@@ -40,7 +47,7 @@ then
     do
         time python pipeline.py --input_dir /data/leslie/suny4/processed_input/ \
             --pred_dir /data/leslie/suny4/predictions/chromafold/ \
-            --paired --ct darko_wt_cb darko_ctcfhet_cb \
+            --paired --ct darko_ctcfhet_cb darko_wt_cb \
             --chrom $i --avg_stripe --kernel tad_diff \
             --min_dim 25 --max_dim 75 --num_dim 10 --close 10 \
             --filters gene_type=protein_coding \
@@ -53,12 +60,25 @@ then
     do
         time python pipeline.py --input_dir /data/leslie/suny4/processed_input/ \
             --pred_dir /data/leslie/suny4/predictions/chromafold/ \
-            --paired --ct darko_wt_cb darko_arid1ahet_cb \
+            --paired --ct darko_arid1ahet_cb darko_wt_cb \
             --chrom $i --avg_stripe --kernel tad_diff \
             --min_dim 25 --max_dim 75 --num_dim 10 --close 10 \
             --filters gene_type=protein_coding \
             --db_file /data/leslie/suny4/data/chrom_size/gencode.vM10.basic.annotation.db \
             --num_plot 10 --out_dir /data/leslie/suny4/pipeline_result/darko_arid1ahet_cb
+    done
+elif [[ "$sample" == "darko_arid1ahet_ctcfhet_cb" ]]
+then
+    for (( i=$start; i<=$end; i++ ))
+    do
+        time python pipeline.py --input_dir /data/leslie/suny4/processed_input/ \
+            --pred_dir /data/leslie/suny4/predictions/chromafold/ \
+            --paired --ct darko_arid1ahet_cb darko_ctcfhet_cb \
+            --chrom $i --avg_stripe --kernel tad_diff \
+            --min_dim 25 --max_dim 75 --num_dim 10 --close 10 \
+            --filters gene_type=protein_coding \
+            --db_file /data/leslie/suny4/data/chrom_size/gencode.vM10.basic.annotation.db \
+            --num_plot 10 --out_dir /data/leslie/suny4/pipeline_result/darko_arid1ahet_ctcfhet_cb
     done
 else
     echo "No valid commands match, please try again"
